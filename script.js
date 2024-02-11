12805303e907dda1aa103731492db80927e24545
aleksandarBG94 <146056219+aleksandarBG94@users.noreply.github.com>
2024-02-11 14:51:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export function submitPoll() {
+export function submitPoll(db) {
     const selectedOption = document.querySelector('input[name="poll"]:checked');
     const voteMessageContainer = document.getElementById('voteMessage');
 
@@ -23,7 +23,7 @@ export function displayResults() {
     votesRef.on('value', snapshot => {
         const votes = snapshot.val();
         let resultText = 'Резултати:<br><br>';
-        const options = ['Уиски', 'Вино', 'Ракия', 'Джин', 'Ром', 'Бира'];
+        const options = ['Уиски', 'Вино', 'Ракия', 'Джин', 'Ром', 'Бира']; // Add all the options you have
         options.forEach(option => {
             const voteCount = votes && votes[option] ? votes[option] : 0;
             resultText += `${option}: ${voteCount} votes<br>`;
